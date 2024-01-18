7c04a987390e563ba354d218b4213c1e118a950b
Niklas Röttger <niklas.roettger@outlook.de>
2024-01-18 09:57:44 +0100

    add demo code

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,14 +1,35 @@
 import Link from "next/link";
+import { movies } from "@/lib/data";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function Movies() {
+  const router = useRouter();
   return (
     <>
+      <Head>
+        <title>List of Movies</title>
+      </Head>
       <h1>List of Movies</h1>
       <ul>
-        <li>
-          <Link href={`/movies/aquaman`}>Aquaman</Link>
-        </li>
+        {movies.map((movie) => {
+          return (
+            <li key={movie.id}>
+              <Link href={`/movies/${movie.slug}`}>{movie.title}</Link>
+            </li>
+          );
+        })}
       </ul>
+      <button
+        type="button"
+        onClick={() => {
+          if (confirm("Are you Aquaman?")) {
+            router.push("/movies/aquaman");
+          }
+        }}
+      >
+        Are you Aquaman?
+      </button>
     </>
   );
 }
